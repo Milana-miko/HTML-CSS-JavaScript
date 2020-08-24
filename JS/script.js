87351817b7435d6413475ae49763cca424ab3b04
Milana-miko <milanamiko88@gmail.com>
2020-08-24 14:51:56 +0400

    Initial commit

---
+++ JS/script.js
@@ -0,0 +1,89 @@
+//alert("Barev es Milanan em");
+/*var a = 5;
+var b = "Barev";
+var c = false;
+let myNumber = 6;
+let myName = "Milana";
+const myAnswer = true;
+const myNumber1 = 15;
+console.log(typeof(myName));
+
+
+console.log(a + myNumber);
+console.log(b + " " + myName);
+console.log(a + myAnswer);
+
+
+const q = prompt("What is your name?")
+//console.log(q);
+//alert(q + " " + ", how are you doing?");
+
+
+//console.log(myNumber / 9);
+
+if(q === "Miko"){
+    alert("You are my friend, " + q);
+}
+
+else if(q === "Ani"){
+    alert("Hello!!");
+}
+
+else {
+    alert("I don't know you");
+}
+
+
+const myArr = ['red', 'blue', 'black', 'yellow', 'white'];
+console.log(myArr.length);
+
+for(let i = 0; i < myArr.length; i++){
+    console.log(i + 1 + '. ' + myArr[i]);
+}
+
+/* let i = 0; i++
+0
+i += 1; // i = i + 1
+2
+i += 3; // i = i + 3
+5
+0/0
+NaN
+1/'Milana'
+NaN
+-1/0
+-Infinity
+1/0
+Infinity
+
+NaN === NaN
+false */
+
+//Primitive type
+/*Number
+String
+Boolean
+Null  // mer koxmic sahmanvac datark popoxakan
+Undefined // menk chenk sahmanel bayc porcum enk ogtagorcel*/
+
+/*console.log(milana)
+VM1948:1 Uncaught ReferenceError: milana is not defined
+    at <anonymous>:1:13
+(anonymous) @ VM1948:1
+console.log(milana);
+var milana = 'Barev';
+VM2128:1 undefined
+undefined
+
+hoisting // var depqum sahmanumy barcacrnum e verev, let u const depqum da chi gorcum*/
+
+let myString = 'Hello';
+let myString1 = myString;
+myString = 'Hello1';
+console.log(myString1);
+
+
+let myArr = [1, 'Ani', false, 12, 'Collor'];
+let myNewArr = myArr;
+myArr[2] = true;
+console.log(myNewArr);
